e1ee2efa6d6704938441e4e38aa5bdb710ce1e1c
weixuansun <weixuansun7@gmail.com>
2021-10-19 20:36:39 +1100

    add a readme file

--- infer_cam.py
+++ infer_cam.py
@@ -20,7 +20,6 @@ from datetime import datetime
 import time
 
 
-
 classes = ['aeroplane','bicycle','bird','boat','bottle','bus','car',
            'cat','chair','cow','diningtable','dog','horse','motorbike',
            'person','pottedplant','sheep','sofa', 'train','tvmonitor']
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+
+
+classification weights: https://1drv.ms/u/s!Ak3sXyXVg7818CLKis4D2CXKXV6D?e=0k9HWo
+mass weight link: https://1drv.ms/u/s!Ak3sXyXVg7818CNWbKYlQ9ny-UuL?e=XANemt
+splitted image link: https://1drv.ms/u/s!Ak3sXyXVg7818CSGY3V0Th4hZiak?e=bpaqkw
